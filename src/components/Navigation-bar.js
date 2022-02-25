03e85b15283899be916c4a867d68b2d36dde3c84
Alejandro Bernal <alejandro.bernal@urbantxt.com>
2022-02-24 20:57:40 -0800

    First commit

---
+++ src/components/Navigation-bar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navigation() {
+    return (
+        <ul>
+            <li>
+                <NavLink to="/">
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/About">
+                    About
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/Contact">
+                    Contact
+                </NavLink>
+            </li>
+        </ul>
+    );
+}
+
+export default Navigation;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import Routers
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// Components
 import App from './App';
+import Navigation from './components/Navigation-bar.js';
+import Home from './components/Home/Home.js';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+// Web vitals
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
